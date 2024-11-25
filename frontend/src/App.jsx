817f13cd3901b6830d8e3548d0e47ddd2272c0f0
KathiravansCode <kathiravanprogrammer@gmail.com>
2024-11-25 11:38:14 +0530

    modified version

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import CreateBook from './pages/CreateBooks';
-import ShowBook from './pages/ShowBook';
-import EditBook from './pages/EditBook';
-import DeleteBook from './pages/DeleteBook';
+import Home from './Pages/Home';
+import ShowBook from './Pages/Showbook';
+import CreateBooks from './Pages/CreateBooks';
+import EditBook from './Pages/Editbook';
+import DeleteBook from './Pages/Deletebook';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/books/create' element={<CreateBooks />} />
       <Route path='/books/details/:id' element={<ShowBook />} />
       <Route path='/books/edit/:id' element={<EditBook />} />
       <Route path='/books/delete/:id' element={<DeleteBook />} />
